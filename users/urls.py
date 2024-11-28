d7a7aa14bbef1d1e48e48f55d53270bec4c5effd
Tatyana Kavardaeva <tatyana.kavardaeva@yandex.ru>
2024-11-28 19:52:33 +0300

    updated users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,16 +2,17 @@ from django.urls import path
 from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from users.apps import UsersConfig
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from users.views import UserViewSet, RegistrationView
+from rest_framework_simplejwt.views import TokenRefreshView
+from users.views import UserViewSet, CustomTokenObtainPairView
 
 app_name = UsersConfig.name
 
 router = SimpleRouter()
-router.register('', UserViewSet)
+router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+
+    path('login/', CustomTokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token-refresh'),
+
 ] + router.urls
