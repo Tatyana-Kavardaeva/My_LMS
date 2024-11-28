792a8145bf35e350dea50b5ff73058d9c2358541
Tatyana Kavardaeva <tatyana.kavardaeva@yandex.ru>
2024-11-28 19:51:01 +0300

    add permissions.py

---
+++ users/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+
+class IsAdmin(permissions.BasePermission):
+    """ Проверяет, является ли пользователь администратором. """
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='Admin').exists()
+
+
+class IsTeacher(permissions.BasePermission):
+    """ Проверяет, является ли пользователь преподавателем. """
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='Teacher').exists()
+
+
+class IsStudent(permissions.BasePermission):
+    """ Проверяет, является ли пользователь студентом. """
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='Student').exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """ Проверяет, является ли пользователь владельцем объекта. """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class IsUser(permissions.BasePermission):
+    """ Разрешение, которое позволяет пользователю редактировать только свой профиль. """
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
